02292fa5c3c282ce5a2dabd170b1764ef1d3f46f
Eric Roe <eric.roe@outlook.com>
2018-12-09 23:39:50 -0800

    Refactoring day 10.
    Much easier to read, and only constructs the grid when we want to print
    it out making the whole process a lot faster

--- day10/day10.py
+++ day10/day10.py
@@ -1,5 +1,3 @@
-from collections import defaultdict, deque
-
 def pos(p):
     return (p[0] + t * p[2], p[1] + t * p[3]) # x,y
 
@@ -7,25 +5,43 @@ with open('./input.txt') as f:
     data = [line for line in f]
 
 d = [(int(l[l.index('<')+1:l.index(',')]), int(l[18:24].strip()), int(l[36:38]), int(l[40:42])) for l in data]
+
+# uncomment to parse test input.  Needs regex refactoring :P
 #d = [(int(l[l.index('<')+1:l.index(',')]), int(l[14:16].strip()), int(l[28:30]), int(l[32:34])) for l in data]
 
-t = 10300
+# Tweak to find solution. 
+# Bump t to help zero in on the solution
+# Size controls the bounding box. Print grid only when it fits inside
+t = 0
+size = 10
+log_interval = 1000
+
 while True:
     gridpts = [pos(p) for p in d]
-    row = ['.'] * (max(p[0] for p in gridpts) + 1 + abs(min(p[0] for p in gridpts)))
-    grid = [row.copy() for i in range(max(p[1] for p in gridpts) + 1 + abs(min(p[1] for p in gridpts)))]
 
-    for p in gridpts:
-        grid[p[1]+abs(min(p[1] for p in gridpts))][p[0]+abs(min(p[0] for p in gridpts))] = '#'
+    minx = min(p[0] for p in gridpts)
+    maxx = max(p[0] for p in gridpts)
+    miny = min(p[1] for p in gridpts)
+    maxy = max(p[1] for p in gridpts)
 
-    print(t, len(grid), len(grid[0]))
-    if 274 > len(grid) > 270:
+    if not t % log_interval:
+        print(t)
+        print([minx, maxx, miny, maxy])
+
+    if maxx - minx < size and maxy - miny < size:
+        print(t)
+        grid = []
+        for y in range(miny, maxy+1):
+            row = []
+            for x in range(minx, maxx+1):
+                if (x,y) in gridpts:
+                    row.append('#')
+                else:
+                    row.append('.')
+            grid.append(row)
+                
         for row in grid:
-            print(''.join(row[:220]))
-        for row in grid:
-            print(''.join(row[220:]))
-        print()
+            print(''.join(row))
 
     t += 1
-
 
