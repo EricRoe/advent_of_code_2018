3c61f751e3398aa5c1ba420516b2f9f963e679f3
EricRoe <eric.roe@outlook.com>
2018-12-20 15:28:50 -0800

    Working day 20
    Didn't realize that I over engineered the solution, so this works for all inputs, not just the ones given as input.

--- day20/day20.py
+++ day20/day20.py
@@ -6,12 +6,14 @@ class Room:
         self.x = x
         self.y = y
         self.dist = 1e9
-        #self.adj = []
+        self.adj = []
 
 
-    def __repr__(self):
-        #return '{}:{}:{}:{}'.format(self.x, self.y, [(r.x, r.y) for r in self.adj], self.dist)
-        return '{}:{}:{}'.format(self.x, self.y,  self.dist)
+    def __repr__(self, d = False):
+        if d:
+            return '{},{}:{}:{}'.format(self.x, self.y, [(r.x, r.y) for r in self.adj], self.dist)
+        else:
+            return '{},{}:{}'.format(self.x, self.y, [(r.x, r.y) for r in self.adj])
 
 
 def add_room(curr, d):
@@ -22,24 +24,27 @@ def add_room(curr, d):
         new_room = Room(pt[0], pt[1])
         floor[pt] = new_room
 
-    #if curr not in new_room.adj:
-        #new_room.adj.append(curr)
-    #if new_room not in curr.adj:
-        #curr.adj.append(new_room)
-    new_room.dist = min(curr.dist + 1, new_room.dist)
+    if curr not in new_room.adj:
+        new_room.adj.append(curr)
+    if new_room not in curr.adj:
+        curr.adj.append(new_room)
 
     return new_room
 
 
-def p():
+def p(d=False):
     for r in floor.values():
-        print(r)
+        print(r.__repr__(d))
     print('='*10)
 
 
 with open('./input.txt') as f:
     r = f.read()
-#r = '^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$'
+
+debug = False
+if debug:
+    r = '(N|E|S|W)'*10
+r = '(N|E|S|W)'*10
 
 
 directions = {
@@ -55,14 +60,17 @@ head_rooms = [start]
 staged_rooms = []
 stack = []
 
+# Build Map
 for i, c in enumerate(r):
-    if not i % 10:
+    print(len(head_rooms))
+    #if len(head_rooms) > 1:
+    if debug:
         print('='*10)
-        print(100*i//len(r))
-        print('hr', len(head_rooms))
-        print('s',len(stack))
-        print('sr',len(staged_rooms))
-    #p()
+        print(c)
+        print('hr', head_rooms)
+        print('s',stack)
+        print('sr',staged_rooms)
+        #p()
     if c in directions:
         for h in range(len(head_rooms)):
             head_rooms[h] = add_room(head_rooms[h], c)
@@ -73,7 +81,7 @@ for i, c in enumerate(r):
 
     elif c == '|':
         staged_rooms[-1] += head_rooms
-        head_rooms = stack[-1]
+        head_rooms = stack[-1].copy()
 
     elif c == ')':
         head_rooms += staged_rooms.pop()
@@ -81,4 +89,15 @@ for i, c in enumerate(r):
         stack.pop()
 
 
+# Fill Map Distances
+stack = [floor[(0,0)]]
+while stack:
+    cur = stack.pop()
+    for r in [r for r in cur.adj if r.dist > cur.dist+1]:
+        r.dist = cur.dist + 1
+        stack.append(r)
+
+
 print(max(r.dist for r in floor.values()))
+
+print(sum(r.dist >= 1000 for r in floor.values()))
